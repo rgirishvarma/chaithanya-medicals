f1000c111c0257fb64a06747ead8f55a6cec8547
rgirishvarma <rvgvarma19@gmail.com>
2026-01-29 06:51:36 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,7 @@
-const CACHE_NAME = "chaithanya-medicals-v1";
-
-const FILES_TO_CACHE = [
-  "index.html",
-  "telugu.html",
-  "english.html",
-  "manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
-    )
-  );
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
 });
